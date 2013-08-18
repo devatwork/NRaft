a003f157e3d5270d3c968a0a94cd216bbb606678
Bert Willems <b.willems@premotion.nl>
2013-08-18 18:21:51 +0200

    + log entry class

---
+++ NRaft.Core/LogEntry.cs
@@ -0,0 +1,17 @@
+﻿namespace NRaft.Core
+{
+	/// <summary>
+	/// Represents a single log entry.
+	/// </summary>
+	public class LogEntry
+	{
+		/// <summary>
+		/// Gets/Sets the term when entry was received by leader.
+		/// </summary>
+		public long Term { get; set; }
+		/// <summary>
+		/// Gets/Sets the position of entry in the log.
+		/// </summary>
+		public long CommitIndex { get; set; }
+	}
+}
