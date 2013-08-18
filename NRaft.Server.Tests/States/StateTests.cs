3807d775cde47974f8e99ec4aa1915c20958e795
Bert Willems <b.willems@premotion.nl>
2013-08-18 20:13:07 +0200

    + state tests

---
+++ NRaft.Server.Tests/States/StateTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NRaft.Server.States;
+using NUnit.Framework;
+
+namespace NRaft.Server.Tests.States
+{
+	[TestFixture]
+	public class StateTests
+	{
+		private class TestState : State
+		{
+			public TestState(ConsensusServer server) : base(server)
+			{
+			}
+		}
+		[Test]
+		public void Constructor()
+		{
+			// assert
+			Assert.That(() => new TestState(null), Throws.InstanceOf<ArgumentNullException>());
+		}
+	}
+}
